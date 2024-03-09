75a65420a54c7ee52c1f81e3663a541affe6d55b
yeisonagm <garciamonsefu@gmail.com>
2024-03-09 17:26:28 -0500

    Crea la estructura del microservicio de autenticación y autorización para el microservicio de eventos

---
+++ src/main/java/edu/unc/auth_eventos/AuthEventosApplication.java
@@ -0,0 +1,13 @@
+package edu.unc.auth_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/unc/auth_eventos/AuthEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unc.auth_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
