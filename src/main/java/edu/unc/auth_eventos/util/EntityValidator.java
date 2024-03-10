73b3f2429bc49dfedab22aa42e474ba0b4fe171a
yeisonagm <garciamonsefu@gmail.com>
2024-03-10 01:08:40 -0500

    Define el model mapper y el método para las validaciones de los DTOs

---
+++ src/main/java/edu/unc/auth_eventos/util/EntityValidator.java
@@ -0,0 +1,32 @@
+/**
+ * @file: Usuario.java
+ * @author: (c)2024 Yeison García
+ * @created: Mar 09, 2024 12:13:47 PM
+ */
+package edu.unc.auth_eventos.util;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Clase que se encarga de validar las entidades y devolver los errores de validación.
+ */
+public class EntityValidator {
+    /**
+     * Método que recibe un resultado de validación y devuelve un ResponseEntity con los errores.
+     *
+     * @param result Resultado de validación.
+     * @return ResponseEntity con los errores de validación.
+     */
+    public ResponseEntity<Map<String, String>> validate(BindingResult result) {
+        Map<String, String> errores = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), "Error [" + error.getField() + "]: " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
+    }
+}
