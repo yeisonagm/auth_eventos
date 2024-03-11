6c506ecb05e5b40287a746138caabeb4327d66a4
yeisonagm <garciamonsefu@gmail.com>
2024-03-10 23:10:26 -0500

    Personaliza el error 401 y la clase `UserDetail` con la entidad usuario

---
+++ src/main/java/edu/unc/auth_eventos/config/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package edu.unc.auth_eventos.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import edu.unc.auth_eventos.entity.Usuario;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    private final Usuario usuario;
+
+    public CustomUserDetails(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(usuario.getRol().getNombre()));
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/edu/unc/auth_eventos/exception/GlobalExceptionHandler.java
+++ src/main/java/edu/unc/auth_eventos/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package edu.unc.auth_eventos.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,40 +16,53 @@ public class GlobalExceptionHandler {
     /**
      * Maneja la excepción EntityNotFoundException.
      *
-     * @param ex la excepción lanzada.
+     * @param ex      la excepción lanzada.
      * @param request la solicitud web que resultó en una excepción.
      * @return una respuesta HTTP personalizada con detalles de error.
      */
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMessage> resourceNotFoundException(EntityNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage(),request.getDescription(false));
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
     /**
      * Maneja la excepción IllegalOperationException.
      *
-     * @param ex la excepción lanzada.
+     * @param ex      la excepción lanzada.
      * @param request la solicitud web que resultó en una excepción.
      * @return una respuesta HTTP personalizada.
      */
     @ExceptionHandler(IllegalOperationException.class)
     public ResponseEntity<?> handleIllegalOperationException(IllegalOperationException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(HttpStatus.BAD_REQUEST,ex.getMessage(),request.getDescription(false));
+        ErrorMessage message = new ErrorMessage(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
     /**
      * Maneja la excepción generales.
      *
-     * @param ex la excepción general.
+     * @param ex      la excepción general.
      * @param request la solicitud web asociada.
      * @return una respuesta HTTP personalizada con detalles de error.
      */
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request){
-        ErrorMessage message = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR,ex.getMessage(),request.getDescription(false));
+    public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Maneja la excepción BadCredentialsException.
+     *
+     * @param ex      la excepción lanzada.
+     * @param request la solicitud web que resultó en una excepción.
+     * @return una respuesta HTTP personalizada.
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorMessage> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(HttpStatus.UNAUTHORIZED, ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
     }
 }
 
